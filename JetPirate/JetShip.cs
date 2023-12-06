d4ecf14a3e03069cffcc03eb725291ec6fcaaf40
Iurii Kupreev <74458480+Kostishko@users.noreply.github.com>
2023-12-06 13:06:44 +0000

    Rotation inertion

--- JetPirate/JetShip.cs
+++ JetPirate/JetShip.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Microsoft.VisualBasic.Logging;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -18,15 +19,10 @@ namespace JetPirate
 
         //Movement variables (there are variable of real velocity and plan velocity to create the effect of inertion)
         //Rotation heritage from Object2D
-        private float planRotation;
-        protected float PlanRotation
-        {
-            get => planRotation;
-            set
-            {
-                planRotation = Object2D.ModulasClamp(value, (float)-Math.PI * 2, (float)Math.PI * 2);
-            }
-        } // rotation that 
+        private float rightRotation;
+        private float leftRotation;
+
+        
         private float maxPower; //Max power for both engines
         //engines' powers. Clamp as half of maxPower. Sum them to final power;
         private float leftPower;
@@ -82,7 +78,8 @@ namespace JetPirate
             texture = tex;
             origin = new Vector2(tex.Width / 2, tex.Height / 2);
             maxGravity = 2f;
-            maxPower = 4f;
+            maxPower = 6f;
+
         }
 
 
@@ -91,7 +88,11 @@ namespace JetPirate
             //Rotate and increse power with Triggers and decrease that if Triggers released
             if(gPad.Triggers.Left!=0)
             {
-                PlanRotation -= 0.02f*gPad.Triggers.Left;
+                if (gPad.Triggers.Right == 0)
+                {
+                    leftRotation -= 0.02f * gPad.Triggers.Left;
+                }
+                //Rotation -= 0.02f*gPad.Triggers.Left;
                 LeftPower += 0.01f * gPad.Triggers.Left;
             }
             else
@@ -101,7 +102,11 @@ namespace JetPirate
 
             if(gPad.Triggers.Right!=0) 
             {
-                PlanRotation += 0.02f * gPad.Triggers.Right;
+                if (gPad.Triggers.Left == 0)
+                {
+                    rightRotation += 0.02f * gPad.Triggers.Right;
+                }
+                //Rotation += 0.02f * gPad.Triggers.Right;
                 RightPower+=0.01f* gPad.Triggers.Right;
             }
             else
@@ -110,25 +115,61 @@ namespace JetPirate
             }
 
             //compute inertion of rotation
-            if (Rotation!=PlanRotation)
+            //if (Rotation!=PlanRotation)
+            //{
+            //    if(Rotation>PlanRotation)
+            //    {
+            //        Rotation = Math.Clamp(Rotation-(0.08f),PlanRotation,(float)Math.PI);
+            //    }
+            //    else
+            //    {
+            //        Rotation = Math.Clamp(Rotation+(0.08f),-(float)Math.PI, PlanRotation );
+            //    }
+            //}
+            if (leftRotation<0)
             {
-                if(Rotation>PlanRotation)
-                {
-                    Rotation = Math.Clamp(Rotation-(0.1f/currentPower),PlanRotation,(float)Math.PI*2);
-                }
-                else
-                {
-                    Rotation = Math.Clamp(Rotation+(0.1f/currentPower),-(float)Math.PI * 2, PlanRotation );
-                }
+                Rotation -= 0.015f;
+                leftRotation += 0.015f;
             }
+            if(rightRotation>0)
+            {
+                Rotation += 0.015f;
+                rightRotation -= 0.015f;
+            }
+            
 
             //Gravitation compute
-            CurrentGravity = maxGravity/ currentPower;
+            CurrentGravity = maxGravity;
 
             // velocity that create player
             planVelocity = new Vector2((float)Math.Sin(Rotation) * currentPower, - (float)Math.Cos(Rotation) * currentPower + CurrentGravity);
+            
 
-            position += planVelocity;
+            if (velX != planVelocity.X)
+            {
+                if (velX > planVelocity.X)
+                {
+                    velX = Math.Clamp(velX - 0.05f, planVelocity.X - 0.02f, 10);
+                }
+                else
+                {
+                    velX = Math.Clamp(velX + 0.05f, -100, planVelocity.X + 0.02f);
+                }
+            }
+            if (velY != planVelocity.Y)
+            {
+                if (velY > planVelocity.Y)
+                {
+                    velY = Math.Clamp(velY - 0.05f, planVelocity.Y - 0.02f, 10);
+                }
+                else
+                {
+                    velY = Math.Clamp(velY + 0.05f, -100, planVelocity.Y + 0.02f);
+                }
+            }
+            velocity = new Vector2(velX, velY);
+
+            position += velocity;
         }
 
         public void DrawMe(SpriteBatch sp)
@@ -136,7 +177,8 @@ namespace JetPirate
             sp.Draw(texture, position, null, Color.White, Rotation, origin, 0.1f, SpriteEffects.None, 1f);
             DebugManager.DebugString("current power: " + currentPower, new Vector2(0, 0));
             DebugManager.DebugString("Rotation: "+ Rotation, new Vector2(0, 22));    
-            DebugManager.DebugString("Plan rotation: "+PlanRotation, new Vector2(0, 44));
+            //DebugManager.DebugString("Planotation: "+PlanRotation, new Vector2(0, 44));
+            DebugManager.DebugString("Origin: " + origin, new Vector2(0, 66));
         }
     }
 }
--- JetPirate/Object2D.cs
+++ JetPirate/Object2D.cs
@@ -15,7 +15,7 @@ namespace JetPirate
             get => rotation;
             set
             {
-                rotation = ModulasClamp(value, (float)-Math.PI * 2, (float)Math.PI * 2);
+                rotation = ModulasClamp(value, (float)-Math.PI, (float)Math.PI);
             }
         }
 
@@ -40,12 +40,12 @@ namespace JetPirate
             float ret;
             if (value >= max)
             {
-                ret = min + value % (max - min);
+                ret = min + value % (max);
                 return ret;
             }
             else if (value < min)
             {
-                ret = (max) - Math.Abs(value % (max - min));
+                ret = (max) - Math.Abs(value % (max));
                 return ret;
             }
 
